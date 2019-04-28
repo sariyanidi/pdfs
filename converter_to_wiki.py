1343f3485bb9ecc98a3a2e85630d634be81fb017
v localmonster <v@localmonster>
2019-04-28 16:06:15 -0400

    Updated matrix notes with Chapter 17

--- converter_to_wiki.py
+++ converter_to_wiki.py
@@ -6,9 +6,15 @@ import re
 
 
 for line in fileinput.input():
+    line = re.sub(r'\$\$([^$]*)\$\$', r'<math>\1</math>', line.rstrip())
     line = re.sub(r'\$([^$]*)\$', r'<math>\1</math>', line.rstrip())
     line = re.sub(r'\\textit\{([^}]*)\}', r"''\1''", line.rstrip())
+    #line = re.sub(r'\\rowsp\{[^}]*\}', r"\\mathcal\{R\}(\\mathbf\1)", line.rstrip())
+    line = re.sub(r'\\rowsp\{([^}]*)\}', r"\\mathcal{R}( \\mathbf \1)", line.rstrip())
+    line = re.sub(r'\\colsp\{([^}]*)\}', r"\\mathcal{C}( \\mathbf \1)", line.rstrip())
     line = re.sub(r'\\mb', r'\\mathbf', line.rstrip())
+    line = re.sub(r'\\mc', r'\\mathcal', line.rstrip())
+    line = re.sub(r'\\rank', r'\\text{rank}', line.rstrip())
     line = re.sub(r'\\begin{itemize}', r'', line.rstrip())
     line = re.sub(r'\\end{itemize}', r'', line.rstrip())
     line = re.sub(r'\\item', r'*', line.rstrip())
