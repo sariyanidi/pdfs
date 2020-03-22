0c3b83ce2fdaa28cc495acf18c4ad895ceedce84
v localmonster <v@localmonster>
2020-03-22 12:09:54 -0400

    some corrections and additions to the convex optimization notes

--- converter_to_wiki.py
+++ converter_to_wiki.py
@@ -8,11 +8,13 @@ import re
 for line in fileinput.input():
     line = re.sub(r'\$\$([^$]*)\$\$', r'<math>\1</math>', line.rstrip())
     line = re.sub(r'\$([^$]*)\$', r'<math>\1</math>', line.rstrip())
+    line = re.sub(r'\\textbf\{([^}]*)\}', r"'''\1'''", line.rstrip())
     line = re.sub(r'\\textit\{([^}]*)\}', r"''\1''", line.rstrip())
     #line = re.sub(r'\\rowsp\{[^}]*\}', r"\\mathcal\{R\}(\\mathbf\1)", line.rstrip())
     line = re.sub(r'\\rowsp\{([^}]*)\}', r"\\mathcal{R}( \\mathbf \1)", line.rstrip())
     line = re.sub(r'\\colsp\{([^}]*)\}', r"\\mathcal{C}( \\mathbf \1)", line.rstrip())
     line = re.sub(r'\\mb', r'\\mathbf', line.rstrip())
+    line = re.sub(r'\\hdots', r'\\dots`', line.rstrip())
     line = re.sub(r'\\mc', r'\\mathcal', line.rstrip())
     line = re.sub(r'\\rank', r'\\text{rank}', line.rstrip())
     line = re.sub(r'\\begin{itemize}', r'', line.rstrip())
